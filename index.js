dcb52421bc519493d3a194c7cf7669c427cc21f7
Cos <cos@anca.im>
2021-11-02 10:50:59 +0000

    Extend recommended-scss

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: [
-    "stylelint-config-recommended",
+    "stylelint-config-recommended-scss",
     "stylelint-config-property-sort-order-smacss",
   ],
   plugins: ["stylelint-scss"],
